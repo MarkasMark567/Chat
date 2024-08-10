4286329646a320f524ffa3930d5e171010d1666c
MarkasMark567 <113364058+MarkasMark567@users.noreply.github.com>
2024-08-10 14:34:17 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 import socket
-(
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.close()
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0', 8080))
-server_socket.listen(5)
 
+# Create a TCP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Confirm socket creation and bind it to the port
+try:
+    server_socket.bind(('0.0.0.0', 8080))
+    print("Socket binded to 0.0.0.0 on port 8080")
+except socket.error as msg:
+    print(f"Bind failed. Error Code : {str(msg[0])} Message {msg[1]}")
+    sys.exit()
+
+server_socket.listen(5)
+print("Socket is now listening")
+
+# Main loop to accept connections
 while True:
     client_socket, addr = server_socket.accept()
     print(f"Connection from {addr}")
