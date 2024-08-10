10729c2240bb577b313c8b997ea8bf1370ca6aa9
MarkasMark567 <113364058+MarkasMark567@users.noreply.github.com>
2024-08-10 14:24:23 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', 8080))
+server_socket.listen(5)
+
+while True:
+    client_socket, addr = server_socket.accept()
+    print(f"Connection from {addr}")
+    client_socket.send(b'Hello, World!')
+    client_socket.close()
