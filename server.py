46d85fe0dd218b120799b8aa31504df6e1061654
MarkasMark567 <113364058+MarkasMark567@users.noreply.github.com>
2024-08-10 14:24:49 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.close()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('0.0.0.0', 8080))
 server_socket.listen(5)
 
